8af55bc59d41c3c1845ff647b893f9a155e8f155
JW-dev0505 <shcherbakarina6@gmail.com>
2024-11-20 21:42:33 +0800

    update

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Unocss(),
+  ],
+  server: {
+    host: '127.0.0.1',
+    port: 3000
+  },
+  base: process.env.NODE_ENV === 'production' ? 'https://chenxch.github.io/xlegex/' : '/',
+})
