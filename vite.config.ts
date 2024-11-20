0e68cffb0468022ea0cefeef18a2fd2780720ac5
JW-dev0505 <shcherbakarina6@gmail.com>
2024-11-20 21:55:25 +0800

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -10,7 +9,7 @@ export default defineConfig({
   ],
   server: {
     host: '127.0.0.1',
-    port: 3000
+    port: 3000,
   },
-  base: process.env.NODE_ENV === 'production' ? 'https://chenxch.github.io/xlegex/' : '/',
+  base: '/',
 })
